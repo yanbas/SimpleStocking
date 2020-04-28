846653dd7e5dc5cf3cfcc6176507dcea0dea21de
yandi <yandi.bastian@gmail.com>
2020-04-28 20:50:40 +0700

    first init

---
+++ src/test/java/com/simplestocking/demo/SimpleStockingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplestocking.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleStockingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
